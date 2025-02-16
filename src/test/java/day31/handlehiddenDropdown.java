4597a0fa694c501dc9f737b2afae48ebac7e3a60
sazidahmednassir <sazidahmednassir@gmail.com>
2025-02-16 22:05:41 +0600

    add hidden dropdown

---
+++ src/test/java/day31/handlehiddenDropdown.java
@@ -0,0 +1,36 @@
+package day31;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class handlehiddenDropdown {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		
+		driver.findElement(By.xpath("//span[normalize-space()='PIM']")).click();
+		
+		driver.findElement(By.xpath("//div[6]//div[1]//div[2]//div[1]//div[1]//div[2]//i[1]")).click();
+		
+		driver.findElement(By.xpath("//span[normalize-space()='Chief Financial Officer']")).click();
+		
+		driver.findElement(By.xpath("//button[normalize-space()='Search']")).click();
+		
+		driver.quit();
+		
+
+	}
+
+}
---
+++ src/test/java/day32/autoSuggestionDemo.java
@@ -0,0 +1,12 @@
+package day32;
+
+public class autoSuggestionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
